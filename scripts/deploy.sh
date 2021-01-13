b3ee9fd28dde1176eff70a29ad9099404e7e6fbd
Rio Kierkels <riokierkels@gmail.com>
2021-01-13 17:06:59 +0100

    feat(scripts): add a preflight check in the deploy script
    This will check if all the binaries are there.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,46 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-set -eu
+set -euo pipefail
 
 cd "$(dirname $0)/.."
 
 GLOBAL_TIMEOUT=5m
 
-echo "### Installing cert-manager"
+function preflight_check() {
+    fail="false"
+
+    printf "Checking if kubectl is installed.\n"
+    if ! command -v kubectl > /dev/null ; then
+        printf "The kubectl binary cannot be found.\n"
+        fail="true"
+    fi
+
+    printf "Checking if kustomize is installed.\n"
+    if ! command -v kustomize > /dev/null ; then
+        printf "The kustomize binary cannot be found.\n"
+        fail="true"
+    fi
+
+    if [ -z "${USE_HELM_OPERATOR:-}" ]; then
+        printf "USE_HELM_OPERATOR not set to 'yes' checking for the helm binary.\n"
+        if ! command -v helm > /dev/null ; then
+            printf "The helm binary cannot be found.\n"
+            fail="true"
+        fi
+    fi
+
+    if $fail = "true" ; then
+        printf "Check that the required tools are installed in your PATH.\n"
+        printf "You can use the 'download-tools.sh' script in the scripts directory to download any missing tools.\n"
+        exit 1
+    fi
+
+    printf "All required binaries found.\n\n"
+}
+
+preflight_check
+
+printf "### Installing cert-manager"
 kustomize build deploy/dependencies/cert-manager | kubectl apply -f -
 
 if [ "${USE_HELM_OPERATOR:-}" = "yes" ]; then
@@ -30,11 +64,15 @@ else
     helm repo update
 
     echo -e "\n#### Installing traefik using helm cli"
-    helm upgrade --install --namespace traefik-system --create-namespace --wait traefik traefik/traefik --version 9.12.3
+    helm upgrade --install --namespace traefik-system --create-namespace traefik traefik/traefik --version 9.12.3
 
     cat > mariadb-values.yaml <<EOF
 auth:
     rootPassword: root
+
+primary:
+    persistence:
+        enabled: false
 
 initdbScripts:
     create_signer_database.sql: |
@@ -49,7 +87,7 @@ initdbScripts:
 EOF
 
     echo -e "\n#### Installing mariadb using helm cli"
-    helm upgrade --install --namespace mariadb --create-namespace --wait mariadb bitnami/mariadb --version 9.2.2 --values mariadb-values.yaml
+    helm upgrade --install --namespace mariadb --create-namespace mariadb bitnami/mariadb --version 9.2.2 --values mariadb-values.yaml
 fi
 
 echo -e "\n### Waiting for cert-manager to report ready"
