0e788847d6bc5b952bdccfae33753f1238d727ea
Rio Kierkels <riokierkels@gmail.com>
2021-01-21 15:34:25 +0100

    chore(scripts): use variables

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -4,26 +4,34 @@ set -euo pipefail
 
 cd "$(dirname $0)/.."
 
+REGISTRY_HOST=localhost
+REPOSITORY_NAME=library/alpine
+
+IMAGE_NAME=${REGISTRY_HOST}/${REPOSITORY_NAME}
+
+UNSIGNED=${REGISTRY_HOST}/${REPOSITORY_NAME}:unsigned
+SIGNED=${REGISTRY_HOST}/${REPOSITORY_NAME}:signed
+
+export DOCKER_CONTENT_TRUST_ROOT_PASSPHRASE='root'
+export DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE='repo'
+
 printf "# Exercising registry\n\n"
 
 printf "## Pulling alpine:3.13 image\n\n"
 
 docker pull alpine:3.13
 
-printf "\n## Tagging alpine:3.13 as localhost/library/alpine:unsigned\n\n"
+printf "\n## Tagging alpine:3.13 as ${UNSIGNED}\n\n"
 
-(set -x; docker tag alpine:3.13 localhost/library/alpine:unsigned )
+(set -x; docker tag alpine:3.13 ${UNSIGNED} )
 
-printf "\n## Pushing localhost/library/alpine:unsigned image\n\n"
+printf "\n## Pushing ${UNSIGNED} image\n\n"
 
-docker push localhost/library/alpine:unsigned
+docker push ${UNSIGNED}
 
 printf "\n# Registry functional\n\n"
 
 printf "# Exercising Notary\n\n"
-
-export DOCKER_CONTENT_TRUST_ROOT_PASSPHRASE='root-passphrase'
-export DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE='repo-passphrase'
 
 if [ -f $(hostname).pub ]; then
     printf "## Reusing $(hostname).pub public key.\n"
@@ -34,17 +42,17 @@ else
     docker trust key generate $(hostname)
 fi
 
-    printf "\n## Adding $(hostname).pub as signer for localhost/library/alpine root passphrase '${DOCKER_CONTENT_TRUST_ROOT_PASSPHRASE}' and repository passphrase '${DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE}'\n\n"
+    printf "\n## Adding $(hostname).pub as signer for ${IMAGE_NAME} root passphrase '${DOCKER_CONTENT_TRUST_ROOT_PASSPHRASE}' and repository passphrase '${DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE}'\n\n"
 
-docker trust signer add --key $(hostname).pub $(hostname) localhost/library/alpine
+docker trust signer add --key $(hostname).pub $(hostname)  ${IMAGE_NAME}
 
-printf "\n## Tagging alpine:3.13 as localhost/library/alpine:signed\n\n"
+printf "\n## Tagging alpine:3.13 as ${SIGNED}\n\n"
 
-(set -x; docker tag alpine:3.13 localhost/library/alpine:signed)
+(set -x; docker tag alpine:3.13 ${SIGNED})
 
-printf "\n## Signing and pushing localhost/library/alpine:signed\n\n"
+printf "\n## Signing and pushing ${SIGNED}\n\n"
 
-docker trust sign localhost/library/alpine:signed
+docker trust sign ${SIGNED}
 
 printf "\n# Notary functional\n\n"
 
@@ -52,24 +60,24 @@ printf "# Exercising Docker Content Trust\n\n"
 
 printf "## Deleting signed and unsigned tags\n\n" 
 
-docker image rm localhost/library/alpine:unsigned localhost/library/alpine:signed
+docker image rm ${UNSIGNED} ${SIGNED}
 
 export DOCKER_CONTENT_TRUST=1
 
-printf "\n## Verifying Docker fails to validate localhost/library/alpine:unsigned\n\n"
+printf "\n## Verifying Docker fails to validate ${UNSIGNED}\n\n"
 
-if docker pull localhost/library/alpine:unsigned ; then
+if docker pull ${UNSIGNED} ; then
     printf "### Docker should have failed pulling and validating this image."
     exit 1
 fi
 
-printf "\n## Verifying Docker succeeds to validate localhost/library/alpine:signed\n\n"
+printf "\n## Verifying Docker succeeds to validate ${SIGNED}\n\n"
 
-docker pull localhost/library/alpine:signed
+docker pull ${SIGNED}
 
 printf "\n## Inspecting signatures\n"
 
-docker trust inspect --pretty localhost/library/alpine:signed
+docker trust inspect --pretty ${SIGNED}
 
 printf "\n# Docker Content Trust functional\n\n"
 
