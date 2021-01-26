128ab4ba027ff32be75966ef306273f06aea8cd1
Rio Kierkels <riokierkels@gmail.com>
2021-01-26 14:03:00 +0100

    chore(play-demo): add comment on how it was recorded

--- scripts/play-demo.sh
+++ scripts/play-demo.sh
@@ -9,4 +9,7 @@ fi
 
 cd "$(dirname $0)/.."
 
+# this was recorded using the script command and doitlive
+# script -T timing typescript -c "doitlive play -q session.sh"
+
 scriptreplay -T live-demo/timing live-demo/typescript
